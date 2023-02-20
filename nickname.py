3bb060cbc1a49115ade9bcaebc6adb69682c9c95
Ivana Pekníková <124450336+slimshadinka@users.noreply.github.com>
2023-02-20 16:07:17 +0100

    nickname.py
    generator random nickname

---
+++ nickname.py
@@ -0,0 +1,9 @@
+import random
+
+alphabet = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
+result = ' '
+
+for i in range(0, 10):
+    result += alphabet[random.randint(0,25)]
+
+print(result)
